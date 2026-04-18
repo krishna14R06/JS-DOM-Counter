3f2a36b45e25967d311a541568578d9bd9484c80
krishna14R06 <krishnarathore1406@gmail.com>
2026-04-18 16:06:58 +0530

    Added Reset button and negative number trap

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let count = 0;
 const displayElement = document.getElementById("counter-display");
 const addButton = document.getElementById("add-btn");
 const subButton = document.getElementById("sub-btn");
+const resButton = document.getElementById("res-btn");
 
 addButton.addEventListener("click", function() {
     count = count + 1;
@@ -9,6 +10,13 @@ addButton.addEventListener("click", function() {
 });
 
 subButton.addEventListener("click", function(){
-    count = count - 1;
-    displayElement.innerText =  count;
+    if (count>0) {
+        count = count - 1;
+        displayElement.innerText =  count;
+    }
 });
+
+resButton.addEventListener("click", function(){
+    count = 0;
+    displayElement.innerText = count;
+})
